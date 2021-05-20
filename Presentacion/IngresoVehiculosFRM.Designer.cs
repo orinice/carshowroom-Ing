6980b58e8278304a83e00085db4664b8dd88e5ed
jhonatan hernandez vallejo <jhonherva@hotmail.com>
2021-05-20 15:47:20 -0500

    prueba

--- Presentacion/IngresoVehiculosFRM.Designer.cs
+++ Presentacion/IngresoVehiculosFRM.Designer.cs
@@ -29,12 +29,32 @@ namespace Presentacion
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(154, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // IngresoVehiculosFRM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(383, 359);
+            this.Controls.Add(this.button1);
+            this.Name = "IngresoVehiculosFRM";
             this.Text = "IngresoVehiculosFRM";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
